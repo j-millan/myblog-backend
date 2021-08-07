3893a4af10c2f678289c0a0343c580b2839ad4cd
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-07 12:28:56 -0400

    register BlogPost, BlogComment and BlogCategory in the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import BlogPost, BlogComment, BlogCategory
+
+admin.site.register(BlogPost)
+admin.site.register(BlogComment)
+admin.site.register(BlogCategory)
