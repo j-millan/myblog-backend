4664b71b7cc0df1b2fc39bdf1aa6895075eca324
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-07 12:28:56 -0400

    create blog comment serializer

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,7 +2,17 @@ from django.contrib.auth.models import User
 
 from rest_framework import serializers
 
+from blog.models import BlogPost, BlogComment
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'name', 'last_name']
+
+class BlogCommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+    
+    class Meta:
+        model = BlogComment
+        fields = ['message', 'author', 'post', 'pub_date', 'upd_date']
