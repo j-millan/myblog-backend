08bf2d7276cfc1ee4d2bdbffb8ca3d433db5fffa
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-07 12:28:56 -0400

    fix various serializer errors

--- blog/serializers.py
+++ blog/serializers.py
@@ -7,7 +7,7 @@ from blog.models import BlogPost, BlogComment, BlogCategory
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'name', 'last_name']
+        fields = ['id', 'username', 'first_name', 'last_name']
 
 class BlogCommentSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
@@ -21,13 +21,16 @@ class BlogCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogCategory
         fields = '__all__'
+
 class BlogPostSerializer(serializers.ModelSerializer):
-    author = UserSerializer()
+    author = UserSerializer(read_only=True)
     thumbnail = serializers.SerializerMethodField('get_post_thumbnail')
+    categories = BlogCategorySerializer(read_only=True, many=True)
+    slug = serializers.SlugField(read_only=True)
     
     class Meta:
         model = BlogPost
-        fields = ['title', 'author', 'introduction', 'body', 'thumbnail', 'pub_date', 'upd_date', 'slug']
+        fields = ['title', 'author', 'introduction', 'body', 'thumbnail', 'pub_date', 'upd_date', 'categories', 'slug']
     
     def get_post_thumbnail(self, post):
         return post.thumbnail.url
