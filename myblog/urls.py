bc8fb2b1bd0e25c94d46fdc2b7ef843fb5663417
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-07 14:14:12 -0400

    create auth app

--- myblog/urls.py
+++ myblog/urls.py
@@ -18,12 +18,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from rest_framework import permissions
-
 from blog import urls as blog_urls
+from auth import urls as auth_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include(blog_urls)),
+    path('auth/', include(auth_urls)),
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
