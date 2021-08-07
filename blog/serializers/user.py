4bb16479861904554dfb477a47817a36eaf3786d
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-07 14:13:46 -0400

    create user serializers

--- blog/serializers/user.py
+++ blog/serializers/user.py
@@ -1,7 +1,71 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
+from django.contrib.auth.password_validation import validate_password
+
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        read_only_fields = ['__all__']
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True, 
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'password', 'password2']
+        read_only_fields = ['id']
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True}
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {'password': 'Password fields did not match'})
+        
+        return attrs
+    
+    def create(self, validated_data):
+        return User.objects.create_user(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password'],
+        )
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
         fields = ['id', 'username', 'first_name', 'last_name']
+        read_only_fields = ['id']
+
+class UserUpdatePasswordSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+    class Meta:
+        model = User
+        fields = ['id', 'password', 'password2']
+        read_only_fields = ['id']
+    
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {'password': 'Password fields did not match'})
+
+        return attrs
+    
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data['password'])
+        return instance
+
