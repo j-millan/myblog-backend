def33f347c9b6f70f956232ef362b70f5a85e06a
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-07 12:28:56 -0400

    create blog post list create view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics, mixins
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from blog.models import BlogPost, BlogComment
+from blog.serializers import BlogPostSerializer, BlogCommentSerializer
+
+class BlogPostListCreate(generics.ListCreateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
