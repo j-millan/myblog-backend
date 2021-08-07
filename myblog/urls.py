b4edeec9ca2ee6648c7bc27d3e61b809c05394c4
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-07 12:28:56 -0400

    uninstall drf yasg

--- myblog/urls.py
+++ myblog/urls.py
@@ -20,23 +20,9 @@ from django.conf.urls.static import static
 
 from rest_framework import permissions
 
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
 from blog import urls as blog_urls
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
-)
-
 urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('', include(blog_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
