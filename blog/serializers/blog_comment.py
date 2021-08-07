e0025639476dffaf1849a4a0e3876d0a46a5fa31
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-07 12:28:56 -0400

    create comment create and comment update serializers

--- blog/serializers/blog_comment.py
+++ blog/serializers/blog_comment.py
@@ -1,11 +1,32 @@
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 from rest_framework import serializers
-from blog.models import BlogComment
+
+from blog.models import BlogPost, BlogComment
 from blog.serializers.user import UserSerializer
 
 class BlogCommentSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
-    post = serializers.PrimaryKeyRelatedField(read_only=True)
+    post_id = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = BlogComment
-        fields = ['message', 'author', 'post', 'pub_date', 'upd_date']
+        fields = ['id', 'message', 'author', 'post_id', 'pub_date', 'upd_date']
+        read_only_fields = ['__all__']
+
+class BlogCommentCreateSerializer(serializers.ModelSerializer):
+    author_id = serializers.ReadOnlyField(source='author.id')
+    post_id = serializers.PrimaryKeyRelatedField(
+        queryset=BlogPost.objects.all(), 
+        source='post'
+    )
+    
+    class Meta:
+        model = BlogComment
+        fields = ['id', 'author_id', 'post_id', 'message']
+
+class BlogCommentUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BlogComment
+        fields = ['id', 'message']
+        read_only_fields = ['id']
