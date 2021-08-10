4ce843280d57e626ed4be988eaf64c8295053d78
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-10 06:47:41 -0400

    create user_login view

--- auth/urls.py
+++ auth/urls.py
@@ -5,6 +5,7 @@ from auth.views import user_view
 app_name = 'auth'
 
 urlpatterns = [
+    path('login', user_view.user_login, name='login'),
     path('users', user_view.UserListCreate.as_view(), name='user_list_create'),
     path('users/<int:pk>', user_view.UserDetailUpdateDelete.as_view(), name='user_detail_update_delete'),
     path('users/update-profile', user_view.UserProfileInformationUpdate.as_view(), name='user_profile_update'),  
--- auth/views/user_view.py
+++ auth/views/user_view.py
@@ -1,13 +1,33 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 from rest_framework import generics
+from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework import response, status
 
 from auth.serializers.user_serializer import UserSerializer, UserCreateSerializer, UserUpdateSerializer
 from auth.serializers.user_profile_serializer import UserProfileSerializer
 from auth.permissions import IsUserAuthenticatedOrReadOnly
 from auth.models import UserProfile
+
+@api_view(['POST'])
+def user_login(request):
+    if request.method == 'POST':
+        username = request.data['username']
+        password = request.data['password']
+        user = authenticate(username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            serializer = UserSerializer(user)
+            return response.Response(serializer.data, status=status.HTTP_200_OK)
+        
+        raise AuthenticationFailed
+
+    return response.Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 class UserListCreate(generics.ListCreateAPIView):
     queryset = User.objects.all()
