cec99b626255d44716d9450aeb9388dd0c294370
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-24 21:55:27 -0400

    add icon field to BlogCategory model

--- blog/models.py
+++ blog/models.py
@@ -59,6 +59,7 @@ class BlogCategory(models.Model):
 		('info', 'info'),
 	]
 	tag_color = models.CharField(max_length=10, choices=color_choices, default='light')
+	icon = models.CharField(max_length=70)
 
 	def __str__(self):
 		return self.name
