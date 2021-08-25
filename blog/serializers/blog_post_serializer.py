c7e1106691ba45277e74b0a513298956f1b66e04
Jhonattan Millan <millanjhonattan0@gmail.com>
2021-08-24 22:04:46 -0400

    change response field name to thumbnail_url

--- blog/serializers/blog_post_serializer.py
+++ blog/serializers/blog_post_serializer.py
@@ -7,14 +7,14 @@ from blog.serializers.blog_category_serializer import BlogCategorySerializer
 
 class BlogPostSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
-    thumbnail = serializers.SerializerMethodField('get_post_thumbnail')
+    thumbnail_url = serializers.SerializerMethodField('get_post_thumbnail')
     categories = BlogCategorySerializer(read_only=True, many=True)
     slug = serializers.SlugField(read_only=True)
 
     class Meta:
         model = BlogPost
         fields = ['id', 'title', 'author', 'introduction', 'body',
-                  'thumbnail', 'pub_date', 'upd_date', 'categories', 'slug']
+                  'thumbnail_url', 'pub_date', 'upd_date', 'categories', 'slug']
         read_only_fields = ['__all__']
 
     def get_post_thumbnail(self, post):
